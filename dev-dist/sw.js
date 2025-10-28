6002802203722f0eb8ba8b5debcb8b3f07ce060c
alex9013 <2122100674@soy.utj.edu.mx>
2025-10-27 23:14:41 -0600

    front 1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0i130jk0gp"
+    "revision": "0.110kfr6322o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
